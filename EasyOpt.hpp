3245471252c7df423e77c6d8a37c49896e400f05
wuqi <wuqi198772@gmail.com>
2021-07-22 11:53:57 +0800

    first

---
+++ EasyOpt.hpp
@@ -0,0 +1,295 @@
+#include "ezOptionParser.hpp"
+#define MAHI_GUI_NO_CONSOLE
+#include <Mahi/Gui.hpp>
+#include <Mahi/Util.hpp>
+
+using namespace mahi::gui;
+using namespace mahi::util;
+using namespace  ez;
+
+class ArgumentParser : public OptionParser, public Application
+{
+public:
+  ArgumentParser() : OptionParser()
+  {
+    std::string fontFile = "fonts/custom.ttf";
+	std::string icoFile = "fonts/ico.ttf";
+    if (CheckFileExistence(fontFile)) {
+      ImFont *pFont = ImGui::GetIO().Fonts->AddFontFromFileTTF(fontFile.c_str(), 18.0f, NULL, ImGui::GetIO().Fonts->GetGlyphRangesChineseFull());
+	  if (CheckFileExistence(icoFile)) {
+		  ImFontConfig config;
+		  config.MergeMode = true;
+		  config.GlyphMinAdvanceX = 13.0f; // Use if you want to make the icon monospaced
+		  static const ImWchar icon_ranges[] = { ICON_MIN_FA, ICON_MAX_FA, 0 };
+		  ImGui::GetIO().Fonts->AddFontFromFileTTF(icoFile.c_str(), 13.0f, &config, icon_ranges);
+	  }
+	  ImGui::GetIO().Fonts->Build();
+      ImGui::GetIO().FontDefault = pFont;
+    }
+  };
+
+  // Override update (called once per frame)
+  void update() override
+  {
+    float x, y;
+    x = 150;
+    y = this->groups.size() * 25.0;
+    ImGui::SetNextWindowSize(ImVec2(x, y), ImGuiCond_FirstUseEver);
+    // App logic and/or ImGui code goes here
+    ImGui::Begin(overview.c_str(), &open, ImGuiWindowFlags_NoCollapse);
+    //ImGui::StyleColorsLight();
+    const float windowScale = 1.2f;
+    ImGui::SetWindowFontScale(windowScale);
+	int i = 1;
+    for (auto it = this->groups.begin(); it != this->groups.end(); ++it,++i) {
+      ImGui::Separator();
+      OptionGroup &og = *it;
+      if(og.flags[0] == "-h")
+        continue;
+      std::string label = "##" + std::to_string(i);
+      ImGuiDataType imtype;
+      switch (og.optType) {
+      case EZ_INT8: {
+        char example = 0;
+        imtype = ImGuiDataType_S8;
+        og.getMinMax(example);
+      }
+      break;
+      case EZ_UINT8: {
+        unsigned char example = 0;
+        imtype = ImGuiDataType_U8;
+        og.getMinMax(example);
+      }
+      break;
+      case EZ_INT16: {
+        short example = 0;
+        imtype = ImGuiDataType_S16;
+        og.getMinMax(example);
+      }
+      break;
+      case EZ_UINT16: {
+        unsigned short example = 0;
+        imtype = ImGuiDataType_U16;
+        og.getMinMax(example);
+      }
+      break;
+      case EZ_INT32: {
+        int example = 0;
+        imtype = ImGuiDataType_S32;
+        og.getMinMax(example);
+      }
+      break;
+      case EZ_UINT32: {
+        unsigned int example = 0;
+        imtype = ImGuiDataType_U32;
+        og.getMinMax(example);
+      }
+      break;
+      case EZ_INT64: {
+        long example = 0;
+        imtype = ImGuiDataType_S64;
+        og.getMinMax(example);
+      }
+      break;
+      case EZ_UINT64: {
+        unsigned long example = 0;
+        imtype = ImGuiDataType_U64;
+        og.getMinMax(example);
+      }
+      break;
+      case EZ_FLOAT: {
+        float example = 0;
+        imtype = ImGuiDataType_Float;
+        og.getMinMax(example);
+      }
+      break;
+      case EZ_DOUBLE: {
+        double example = 0;
+        imtype = ImGuiDataType_Double;
+        og.getMinMax(example);
+      }
+      break;
+      default:
+        ;
+      }
+      switch (og.optType) {
+      case EZ_NOTYPE:
+      case EZ_BOOL: {
+        //add checkbox
+        ImGui::Checkbox(og.help.c_str(), &(og.isSet));
+      }
+      break;
+      case EZ_INT8:
+      case EZ_UINT8:
+      case EZ_INT16:
+      case EZ_UINT16:
+      case EZ_INT32:
+      case EZ_UINT32:
+      case EZ_INT64:
+      case EZ_UINT64:
+      case EZ_FLOAT:
+      case EZ_DOUBLE: {
+        if (og.validValues.size() > 0) {
+          //add combo
+          std::vector<std::string> &vecValid = og.validValues;
+          int nIDX = 0;
+          ImGui::Text(og.help.c_str());
+          ImGuiComboFlags flags = 0;
+          size_t &item_current_idx = og.validIndex;
+          if (ImGui::BeginCombo(label.c_str(), vecValid[item_current_idx].c_str(), flags)) {
+            for (size_t n = 0; n < vecValid.size(); n++) {
+              const bool is_selected = (item_current_idx == n);
+              if (ImGui::Selectable(vecValid[n].c_str(), is_selected))
+                item_current_idx = n;
+              if (is_selected) {
+                ImGui::SetItemDefaultFocus();
+                og.args.resize(1);
+                og.args[0].resize(1);
+                og.args[0][0] = vecValid[n];
+                og.isSet = true;
+              }
+            }
+            ImGui::EndCombo();
+          }
+        } else {
+          //add int drag
+          ImGui::Text(og.help.c_str());
+          if (og.scalaValues.size() == 0)
+            og.scalaValues.push_back(0);
+          if (ImGui::DragScalarN(label.c_str(), imtype, &og.scalaValues[0], og.expectArgs, 1.0f, &og.minValid, &og.maxValid)) {
+            size_t scalaSize = og.scalaValues.size();
+            og.isSet = true;
+            og.args.resize(1);
+            og.args[0].resize(scalaSize);
+            for (size_t i = 0; i < scalaSize; ++i) {
+              const std::string &setvalue = std::to_string(og.scalaValues[i]);
+              og.args[0][i] = setvalue;
+            }
+          }
+        }
+      }
+      break;
+      case EZ_TEXT: {
+        if (og.validValues.size() > 0) {
+          //add combo
+          std::vector<std::string> &vecValid = og.validValues;
+          int nIDX = 0;
+          ImGui::Text(og.help.c_str());
+          ImGuiComboFlags flags = 0;
+          size_t &item_current_idx = og.validIndex;
+          if (ImGui::BeginCombo(label.c_str(), vecValid[item_current_idx].c_str(), flags)) {
+            for (size_t n = 0; n < vecValid.size(); n++) {
+              const bool is_selected = (item_current_idx == n);
+              if (ImGui::Selectable(vecValid[n].c_str(), is_selected))
+                item_current_idx = n;
+              if (is_selected) {
+                ImGui::SetItemDefaultFocus();
+                og.args.resize(1);
+                og.args[0].resize(1);
+                og.args[0][0] = vecValid[n];
+                og.isSet = true;
+              }
+            }
+            ImGui::EndCombo();
+          }
+        } else {
+          //input
+          ImGui::Text(og.help.c_str());
+          if (ImGui::InputText(label.c_str(), &og.defaults)) {
+            og.args.resize(1);
+            og.args[0].resize(1);
+            og.args[0][0] = og.defaults;
+            og.isSet = true;
+          }
+        }
+      }
+      break;
+      case EZ_FILE: {
+        //file
+        ImGui::Text(og.help.c_str());
+        ImGui::InputText(label.c_str(), &og.defaults, ImGuiInputTextFlags_ReadOnly);
+        ImGui::SameLine();
+        std::string iconLabel = ICON_FA_FILE + label;
+        if (ImGui::Button(iconLabel.c_str() )) {
+          if(open_dialog(og.defaults) == DialogOkay) {
+            og.args.resize(1);
+            og.args[0].resize(1);
+            og.args[0][0] = og.defaults;
+            og.isSet = true;
+          }
+        }
+      }
+      break;
+      case EZ_DIR: {
+        //dir
+        ImGui::Text(og.help.c_str());
+        ImGui::InputText(label.c_str(), &og.defaults, ImGuiInputTextFlags_ReadOnly);
+        ImGui::SameLine();
+        std::string iconLabel = ICON_FA_FOLDER_OPEN + label;
+        if (ImGui::Button(iconLabel.c_str())) {
+          if (pick_dialog(og.defaults) == DialogOkay) {
+            og.args.resize(1);
+            og.args[0].resize(1);
+            og.args[0][0] = og.defaults;
+            og.isSet = true;
+          }
+        }
+      }
+      break;
+      case EZ_OUTPUT: {
+        //save
+        ImGui::Text(og.help.c_str());
+        ImGui::InputText(label.c_str(), &og.defaults, ImGuiInputTextFlags_ReadOnly);
+        ImGui::SameLine();
+        std::string iconLabel = ICON_FA_SAVE + label;
+        if (ImGui::Button(iconLabel.c_str())) {
+          if (save_dialog(og.defaults) == DialogOkay) {
+            og.args.resize(1);
+            og.args[0].resize(1);
+            og.args[0][0] = og.defaults;
+            og.isSet = true;
+          }
+        }
+      }
+      break;
+      default:
+        break;
+      }
+    }
+    static bool valid = true;
+    ImGui::Separator();
+    ImGui::InvisibleButton("##invisible", ImVec2(10, 30));
+    ImGui::SameLine(ImGui::GetWindowContentRegionMax().x - 120);
+    if (ImGui::Button("OK", ImVec2(50, 30))) {
+      this->errors = "";
+      if (!checkValid(this->errors)) {
+        valid = false;
+      } else {
+        valid = true;
+        ok = true;
+        quit();
+      }
+    }
+    ImGui::SameLine();
+    if (ImGui::Button("Cancel", ImVec2(60, 30))) {
+      quit();
+    }
+    if(!valid) {
+      ImGui::InputTextMultiline("##errorInfo", &this->errors);
+    }
+    ImGui::End();
+    if (!open)
+      quit();
+  }
+
+  inline virtual bool parse(int argc, const char *const *argv)
+  {
+	if (argc > 1){
+	  return OptionParser::parse(argc, argv);
+	}
+    return ok;
+  }
+protected:
+  bool ok = false;
+  bool open = true;
+};
---
+++ main.cpp
@@ -0,0 +1,75 @@
+﻿#include "EasyOpt.hpp"
+
+#include <iostream>
+
+int main(int argc, const char** argv)
+{
+	ArgumentParser opt;
+	//概要说明
+	opt.overview = "sbi:Make the Soil brightness index product.";
+	//语法,不做说明则将自动生成
+	//opt.syntax = "cli [-h] [-e|-f]  [-t modis|fy3a] -i Arg1,[ArgN] -o Arg";
+	//实例
+	opt.example = "./sbi.exe -i input1.h5,input2.h5,input3.h5 output.hdf";
+	opt.footer = "version:0.9,LIC:See MIT-LICENSE.";
+	////设置命令行选项  -h,--usage,--help 命令将自动设置，无需再设置
+	opt.add(
+		"-s,--short",       // 选项的前导符，可以有多个，使用逗号隔开，
+							//    如果前导符中没有"-"或者 "--"，则认为是无标识的选项
+							//    即不需要前导符，直接传入
+		false,                  // 是否为必须?是则为1
+		1,                  // 指定参数个数，-1用于传入列表,指定-1时最少需要1个参数,界面时建议不用-1.
+		"Batch One Time", // 帮助说明
+		ez::EZ_INT16,           //指定数据类型，指定后将自动判断类型的最大最小值,此后的参数为非必需参数
+		"10",               // 默认值,可以设置为空.
+		"1",                //指定最小值，若不需要则为""
+		"",                 //指定最大值，若不需要则为""
+		"1,10,15,20"        //设定允许值，默认为空，都允许
+	);
+	//无参数选项,命令行中，可以使用"-mc"同时设置两个无参数的选项,默认为unknown类型,用于设置true/false
+	opt.add("-m", false, 0, "Merge Files or Not");
+	//添加uint
+	opt.add("--int",false,1,"Uint ID",EZ_UINT32);
+	//添加float参数，要求输入两个
+	opt.add("--float", false, 2, u8"宽度,高度", EZ_FLOAT, "33.5,44.2");
+	//添加double参数,限制输入范围,添加默认参数
+	opt.add("-d,--double", false, 4, "Longitude ranges", ez::EZ_DOUBLE, "", "-180.0", "180.0");
+	//添加text
+	opt.add("-t,--type", false, 2, "Input Image Type", ez::EZ_TEXT, "fy3a", "", "", "modis,fy3a,zy,landsat");
+
+	//无前导符的参数,输入文件,必须存在,将会放在所有参数后面
+	opt.add("input", true, 1, "Input File.", ez::EZ_FILE);
+	//输出文件
+	opt.add("-o,--output", true, 1, "Output file argument.", ez::EZ_OUTPUT);
+
+	//输出文件夹测试
+	opt.add("-D,--dir", false, 1, "test dir arguments", ez::EZ_DIR);
+	//互斥参数设置：使用逗号隔开一串第一个flag设置，互斥参数必须为可选，否则将出现逻辑问题
+	//opt.xorAdd("-b,-f");
+
+	//带参数传入,则使用CLI解析
+	if (argc > 1 && !opt.parse(argc, argv))
+	{
+		return -1;
+	}
+	else
+	{
+	//不带参数传入,则弹出GUI解析
+		opt.run();
+		if (!opt.parse(argc, argv))
+			return -1;
+	}
+	//get args
+
+	//processing
+	try {
+		//start processing
+		//end processing
+	}
+	catch (...) {
+
+	}
+
+	getchar();
+	return 0;
+}
